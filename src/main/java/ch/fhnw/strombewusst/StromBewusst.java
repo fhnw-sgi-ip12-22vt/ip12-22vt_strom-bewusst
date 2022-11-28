23af82fbec56aac32e2743b41e1f2b07d4b241d5
Samuel Burkhardt <samuel.burkhardt@students.fhnw.ch>
2022-11-28 15:31:28 +0100

    Added Pi4J Template pom.xml and run configs
    With these configurations we can now run our project on CrowPi OS, without any errors.

--- src/main/java/ch/fhnw/strombewusst/StromBewusst.java
+++ src/main/java/ch/fhnw/strombewusst/StromBewusst.java
@@ -8,7 +8,7 @@ import com.almasb.fxgl.app.scene.SceneFactory;
 import com.almasb.fxgl.dsl.FXGL;
 import com.almasb.fxgl.entity.Entity;
 import javafx.scene.input.KeyCode;
-import org.jetbrains.annotations.NotNull;
+
 
 public class StromBewusst extends GameApplication {
     private static final int PLAYER_SPEED = 5;
@@ -28,7 +28,6 @@ public class StromBewusst extends GameApplication {
         settings.setWidth(1280);
         settings.setHeight(720);
         settings.setSceneFactory(new SceneFactory() {
-            @NotNull
             @Override
             public FXGLMenu newMainMenu() {
                 return new MainMenu();
---
+++ src/main/java/module-info.java
@@ -0,0 +1,27 @@
+open module com.pi4j.mvc {
+     // Pi4J Modules
+    requires com.pi4j;
+    requires com.pi4j.library.pigpio;
+    requires com.pi4j.library.linuxfs;
+    requires com.pi4j.plugin.pigpio;
+    requires com.pi4j.plugin.raspberrypi;
+    requires com.pi4j.plugin.mock;
+    requires com.pi4j.plugin.linuxfs;
+    uses com.pi4j.extension.Extension;
+    uses com.pi4j.provider.Provider;
+
+    // for logging
+    requires java.logging;
+
+    // JavaFX
+    requires javafx.base;
+    requires javafx.controls;
+
+    // FXGL
+    requires com.almasb.fxgl.all;
+
+
+    // Module Exports
+
+
+}
