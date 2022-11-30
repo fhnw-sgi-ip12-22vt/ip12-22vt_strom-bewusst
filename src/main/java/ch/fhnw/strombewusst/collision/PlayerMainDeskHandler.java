ad24c2e2f93bc452a5e8b6916c832faf2e2a8c25
Damjan Mlinar <damjan.mlinar@students.fhnw.ch>
2022-11-30 16:24:27 +0100

    New Puzzlebackground and PlayerMainDeskHandler

--- src/main/java/ch/fhnw/strombewusst/collision/PlayerMainDeskHandler.java
+++ src/main/java/ch/fhnw/strombewusst/collision/PlayerMainDeskHandler.java
@@ -1,12 +1,17 @@
 package ch.fhnw.strombewusst.collision;
 
 import ch.fhnw.strombewusst.EntityType;
-import ch.fhnw.strombewusst.ui.scene.LeaderboardSubScene;
+import ch.fhnw.strombewusst.components.PlayerComponent;
 import ch.fhnw.strombewusst.ui.scene.PuzzleSubScene;
+import com.almasb.fxgl.dsl.FXGL;
 import com.almasb.fxgl.entity.Entity;
+import com.almasb.fxgl.input.UserAction;
 import com.almasb.fxgl.physics.CollisionHandler;
+import javafx.scene.input.KeyCode;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
 
-import static com.almasb.fxgl.dsl.FXGL.getSceneService;
+import static com.almasb.fxgl.dsl.FXGL.*;
 
 public class PlayerMainDeskHandler extends CollisionHandler {
 
@@ -14,7 +19,26 @@ public class PlayerMainDeskHandler extends CollisionHandler {
         super(EntityType.PLAYER, EntityType.MAINDESK);
     }
 
-    protected void onCollisionBegin(Entity player, Entity desk){
-        getSceneService().pushSubScene(new PuzzleSubScene());
+    Entity question;
+    UserAction action = new UserAction("player1 Down") {
+        @Override
+        protected void onAction() {
+            getSceneService().pushSubScene(new PuzzleSubScene());
+        }
+    };
+
+
+
+        protected void onCollisionBegin(Entity player, Entity desk){
+        question = entityBuilder()
+                .at(desk.getX()-30, desk.getY())
+                .view("Q.png")
+                .buildAndAttach();
+        player.getComponent(PlayerComponent.class).setIsNearDesk(true);
+    }
+
+    protected void onCollisionEnd(Entity player, Entity desk){
+        player.getComponent(PlayerComponent.class).setIsNearDesk(false);
+        question.removeFromWorld();
     }
 }
