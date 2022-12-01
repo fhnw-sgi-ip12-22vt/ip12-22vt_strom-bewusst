9dc87c9133235ee281c163d54d8d5f1e546f7963
Samuel Burkhardt <samuel.burkhardt@students.fhnw.ch>
2022-12-01 10:17:10 +0100

    Implement working version of Controller

--- src/main/java/ch/fhnw/strombewusst/StromBewusst.java
+++ src/main/java/ch/fhnw/strombewusst/StromBewusst.java
@@ -6,7 +6,8 @@ import ch.fhnw.strombewusst.collision.PlayerDoorHandler;
 import ch.fhnw.strombewusst.collision.PlayerMainDeskHandler;
 import ch.fhnw.strombewusst.collision.PlayerPlayerHandler;
 import ch.fhnw.strombewusst.components.PlayerComponent;
-import ch.fhnw.strombewusst.input.Player1Controller;
+import ch.fhnw.strombewusst.input.Controller;
+import ch.fhnw.strombewusst.input.pi4jcomponents.helpers.PIN;
 import ch.fhnw.strombewusst.ui.scene.MainMenu;
 import com.almasb.fxgl.app.ApplicationMode;
 import com.almasb.fxgl.app.GameApplication;
@@ -97,7 +98,44 @@ public class StromBewusst extends GameApplication {
      */
     @Override
     protected void initInput() {
-        Player1Controller p1Controller = new Player1Controller();
+        // Initializing the controllers, their input gets processed and the appropriate keypresses get "simulated"
+        // By simulating the keypresses, the programm can be worked on locally by using WASD, but can be controlled
+        // in production with the controller.
+        try {
+            Controller p1Controller = new Controller(0, 1, PIN.D17);
+            Controller p2Controller = new Controller(2, 3, PIN.D6);
+
+            p1Controller.onJoystickRight(() -> getInput().mockKeyPress(KeyCode.D));
+            p1Controller.onJoystickLeft(() -> getInput().mockKeyPress(KeyCode.A));
+            p1Controller.onJoystickHorizontalIdle(() -> {
+                getInput().mockKeyRelease(KeyCode.A);
+                getInput().mockKeyRelease(KeyCode.D);
+            });
+
+            p1Controller.onJoystickUp(() -> getInput().mockKeyPress(KeyCode.W));
+            p1Controller.onJoystickDown(() -> getInput().mockKeyPress(KeyCode.S));
+            p1Controller.onJoystickVerticalIdle(() -> {
+                getInput().mockKeyRelease(KeyCode.W);
+                getInput().mockKeyRelease(KeyCode.S);
+            });
+
+            p2Controller.onJoystickRight(() -> getInput().mockKeyPress(KeyCode.L));
+            p2Controller.onJoystickLeft(() -> getInput().mockKeyPress(KeyCode.J));
+            p2Controller.onJoystickHorizontalIdle(() -> {
+                getInput().mockKeyRelease(KeyCode.L);
+                getInput().mockKeyRelease(KeyCode.J);
+            });
+
+            p2Controller.onJoystickUp(() -> getInput().mockKeyPress(KeyCode.I));
+            p2Controller.onJoystickDown(() -> getInput().mockKeyPress(KeyCode.K));
+            p2Controller.onJoystickVerticalIdle(() -> {
+                getInput().mockKeyRelease(KeyCode.I);
+                getInput().mockKeyRelease(KeyCode.K);
+            });
+        } catch (Exception ignored) {
+            System.out.println("failed to initialize controller, proceeding");
+        }
+
         // player1 Movement
         getInput().addAction(new UserAction("player1 Right") {
             @Override
--- src/main/java/ch/fhnw/strombewusst/input/Controller.java
+++ src/main/java/ch/fhnw/strombewusst/input/Controller.java
@@ -1,5 +1,9 @@
 package ch.fhnw.strombewusst.input;
 
+import ch.fhnw.strombewusst.input.pi4jcomponents.Ads1115;
+import ch.fhnw.strombewusst.input.pi4jcomponents.JoystickAnalog;
+import ch.fhnw.strombewusst.input.pi4jcomponents.helpers.ContinuousMeasuringException;
+import ch.fhnw.strombewusst.input.pi4jcomponents.helpers.PIN;
 import com.pi4j.Pi4J;
 import com.pi4j.context.Context;
 import com.pi4j.library.pigpio.PiGpio;
@@ -11,7 +15,10 @@ import com.pi4j.plugin.pigpio.provider.serial.PiGpioSerialProvider;
 import com.pi4j.plugin.pigpio.provider.spi.PiGpioSpiProvider;
 import com.pi4j.plugin.raspberrypi.platform.RaspberryPiPlatform;
 
-public abstract class Controller {
+import java.util.ArrayList;
+import java.util.List;
+
+public class Controller {
     protected final static PiGpio piGpio = PiGpio.newNativeInstance();
     protected final static Context pi4jContext = Pi4J.newContextBuilder()
             .noAutoDetect()
@@ -21,7 +28,8 @@ public abstract class Controller {
                     return new String[]{};
                 }
             })
-            .add(PiGpioDigitalInputProvider.newInstance(piGpio),
+            .add(
+                    PiGpioDigitalInputProvider.newInstance(piGpio),
                     PiGpioDigitalOutputProvider.newInstance(piGpio),
                     PiGpioPwmProvider.newInstance(piGpio),
                     PiGpioSerialProvider.newInstance(piGpio),
@@ -29,4 +37,79 @@ public abstract class Controller {
                     LinuxFsI2CProvider.newInstance()
             )
             .build();
+
+    protected final static Ads1115 ads1115 = new Ads1115(pi4jContext, 0x01, Ads1115.GAIN.GAIN_4_096V, Ads1115.ADDRESS.GND, 4);
+
+    public JoystickAnalog joystick;
+
+    private List<Runnable> onJoystickRightTasks = new ArrayList<>();
+    private List<Runnable> onJoystickLeftTasks = new ArrayList<>();
+    private List<Runnable> onJoystickHorizontalIdleTasks = new ArrayList<>();
+    private List<Runnable> onJoystickUpTasks = new ArrayList<>();
+    private List<Runnable> onJoystickDownTasks = new ArrayList<>();
+    private List<Runnable> onJoystickVerticalIdleTasks = new ArrayList<>();
+
+    public Controller(int channelXAxis, int channelYAxis, PIN pin) {
+        joystick = new JoystickAnalog(pi4jContext, ads1115, channelXAxis, channelYAxis, 3.3, true, pin);
+
+        joystick.xOnMove(this::handleXMove);
+        joystick.yOnMove(this::handleYMove);
+
+        //joystick.xOnMove(System.out::println);
+        //joystick.xOnMove(System.out::println);
+
+        try {
+            joystick.calibrateJoystick();
+        } catch (ContinuousMeasuringException ignored) {
+            System.out.println("cont. measuring exception, ignoring");
+        }
+        joystick.start(0.05, 10);
+    }
+
+    private void handleXMove(double value) {
+        List<Runnable> tasks;
+        if (value > 0.75) {
+            tasks = onJoystickRightTasks;
+        } else if (value < 0.25) {
+            tasks = onJoystickLeftTasks;
+        } else {
+            tasks = onJoystickHorizontalIdleTasks;
+        }
+        for (Runnable task : tasks) {
+            task.run();
+        }
+    }
+
+    private void handleYMove(double value) {
+        List<Runnable> tasks;
+        if (value > 0.75) {
+            tasks = onJoystickDownTasks;
+        } else if (value < 0.25) {
+            tasks = onJoystickUpTasks;
+        } else {
+            tasks = onJoystickVerticalIdleTasks;
+        }
+        for (Runnable task : tasks) {
+            task.run();
+        }
+    }
+
+    public void onJoystickRight(Runnable task) {
+        onJoystickRightTasks.add(task);
+    }
+    public void onJoystickLeft(Runnable task) {
+        onJoystickLeftTasks.add(task);
+    }
+    public void onJoystickHorizontalIdle(Runnable task) {
+        onJoystickHorizontalIdleTasks.add(task);
+    }
+    public void onJoystickUp(Runnable task) {
+        onJoystickUpTasks.add(task);
+    }
+    public void onJoystickDown(Runnable task) {
+        onJoystickDownTasks.add(task);
+    }
+    public void onJoystickVerticalIdle(Runnable task) {
+        onJoystickVerticalIdleTasks.add(task);
+    }
 }
