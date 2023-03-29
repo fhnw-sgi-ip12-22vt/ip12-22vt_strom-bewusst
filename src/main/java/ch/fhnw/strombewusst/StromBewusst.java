d2f8ded6958a034cd3a5e520b2acc324d1079d70
Viktor Weilenmann <viktor.weilenmann@students.fhnw.ch>
2023-03-29 12:08:25 +0200

    removed font

--- src/main/java/ch/fhnw/strombewusst/StromBewusst.java
+++ src/main/java/ch/fhnw/strombewusst/StromBewusst.java
@@ -282,7 +282,6 @@ public class StromBewusst extends GameApplication {
     @Override
     protected void initUI() {
         var scoreText = getUIFactoryService().newText("", Color.ANTIQUEWHITE,38.0);
-        scoreText.setFont(Font.font ("Verdana", 20));
         scoreText.textProperty().bind(getip("score").asString("Score: %d"));
         addUINode(scoreText,720,60);
     }
