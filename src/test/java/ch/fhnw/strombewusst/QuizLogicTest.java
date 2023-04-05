db6ffec72472f7158825415b8689ab217d199e95
Samuel Burkhardt <samuel.burkhardt@students.fhnw.ch>
2023-04-05 10:10:59 +0200

    remove unused imports to pass checkstyle

--- src/test/java/ch/fhnw/strombewusst/QuizLogicTest.java
+++ src/test/java/ch/fhnw/strombewusst/QuizLogicTest.java
@@ -1,11 +1,13 @@
 package ch.fhnw.strombewusst;
 
+/*
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
+*/
 
 class QuizLogicTest {
     /*
