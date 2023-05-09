d9a9df0ff9531f8d9af562ef9e490f7d52004563
Samuel Burkhardt <samuel.burkhardt@students.fhnw.ch>
2023-05-09 10:03:29 +0200

    improved UI in EndGameSubScene to show controller buttons

--- src/main/java/ch/fhnw/strombewusst/ui/scene/EndGameSubScene.java
+++ src/main/java/ch/fhnw/strombewusst/ui/scene/EndGameSubScene.java
@@ -1,5 +1,6 @@
 package ch.fhnw.strombewusst.ui.scene;
 
+import ch.fhnw.strombewusst.Config;
 import ch.fhnw.strombewusst.Score;
 import ch.fhnw.strombewusst.StromBewusst;
 import ch.fhnw.strombewusst.TeamName;
@@ -7,12 +8,14 @@ import com.almasb.fxgl.dsl.FXGL;
 import com.almasb.fxgl.input.UserAction;
 import com.almasb.fxgl.scene.SubScene;
 import com.almasb.fxgl.texture.Texture;
+import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.input.KeyCode;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
+import javafx.scene.text.Text;
 
 import static com.almasb.fxgl.dsl.FXGL.getAppHeight;
 import static com.almasb.fxgl.dsl.FXGL.getAppWidth;
@@ -32,6 +35,7 @@ public class EndGameSubScene extends SubScene {
         title.getStyleClass().add("title");
 
         Label score = new Label(FXGL.<Score>geto("score").toString());
+        score.setPadding(new Insets(100, 0, 0, 0));
         score.getStyleClass().add("subtitle");
 
         teamNameLabel = new Label();
@@ -39,20 +43,40 @@ public class EndGameSubScene extends SubScene {
         updateTeamNameLabel();
 
         VBox titleVBox = new VBox(title, score, teamNameLabel);
-        titleVBox.setPrefWidth(getAppWidth());
+        titleVBox.setPrefWidth(Config.WIDTH);
         titleVBox.setAlignment(Pos.CENTER);
         titleVBox.setTranslateY(50);
+
+        Texture selectButton = getAssetLoader().loadTexture("red-button-icon-single.png", 68, 68);
+        Label selectText = new Label("Zurück zum Hauptmenü");
+        selectText.getStyleClass().add("small_title");
+        HBox selectHBox = new HBox(selectButton, selectText);
+        selectHBox.setAlignment(Pos.CENTER);
+        selectHBox.setSpacing(20);
+
+        Texture backButton = getAssetLoader().loadTexture("blue-button-icon-single.png", 68, 68);
+        Label backText = new Label("Teamnamen ändern");
+        backText.getStyleClass().add("small_title");
+        HBox backHBox = new HBox(backButton, backText);
+        backHBox.setAlignment(Pos.CENTER);
+        backHBox.setSpacing(20);
+
+        VBox inputsVBox = new VBox(selectHBox, backHBox);
+        inputsVBox.setPrefWidth(Config.WIDTH);
+        inputsVBox.setAlignment(Pos.CENTER);
+        inputsVBox.setTranslateY(420);
 
         btnEnd = new Button("Back");
         btnEnd.getStyleClass().add("main_menu_button");
         btnEnd.setStyle("-fx-text-fill: black;");
         btnEnd.setOnAction(e -> ((StromBewusst) FXGL.getApp()).endGame(teamName.getTeamName()));
-        HBox endHBox = new HBox(btnEnd);
-        endHBox.setPrefWidth(getAppWidth());
-        endHBox.setAlignment(Pos.CENTER);
-        endHBox.setTranslateY(getAppHeight() - 140);
 
-        getContentRoot().getChildren().addAll(bg, titleVBox, endHBox);
+        HBox endHBox = new HBox(btnEnd);
+        endHBox.setPrefWidth(Config.WIDTH);
+        endHBox.setAlignment(Pos.CENTER);
+        endHBox.setTranslateY(Config.HEIGHT - 140);
+
+        getContentRoot().getChildren().addAll(bg, titleVBox, inputsVBox, endHBox);
 
         getInput().addAction(new UserAction("update team name first") {
             @Override
@@ -74,7 +98,7 @@ public class EndGameSubScene extends SubScene {
      * Updates the username label with a new randomly generated username.
      */
     public void updateTeamNameLabel() {
-        teamNameLabel.setText("TeamName: " + teamName.getTeamName());
+        teamNameLabel.setText("Teamname: " + teamName.getTeamName());
     }
 
     @Override
