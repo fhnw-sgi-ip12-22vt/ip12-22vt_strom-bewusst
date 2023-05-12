4b40d09fbeab6544b5dffbb86f8ae4afaa0173e2
Samuel Burkhardt <samuel.burkhardt@students.fhnw.ch>
2023-05-12 08:59:46 +0200

    delete unused classes

--- src/main/java/ch/fhnw/strombewusst/components/DoorComponent.java
+++
@@ -1,9 +0,0 @@
-package ch.fhnw.strombewusst.components;
-
-import com.almasb.fxgl.entity.component.Component;
-
-public class DoorComponent extends Component {
-    public DoorComponent(int state) {
-
-    }
-}
